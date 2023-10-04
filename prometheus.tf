512b2464e74fb1f07daa419fdf2a08d5f33e1651
danci1050 <obilesa@gmail.com>
2023-10-04 18:30:46 +0100

    Update prometheus.tf

--- prometheus.tf
+++ prometheus.tf
@@ -302,6 +302,8 @@ resource "kubernetes_daemonset" "node_exporter" {
 
 
 
+
+
 # Expose the node exporter metrics endpoint using a Kubernetes service
 resource "kubernetes_service" "node_exporter" {
     count = var.enable_monitoring && var.enable_node_monitoring ? 1 : 0
@@ -326,3 +328,270 @@ resource "kubernetes_service" "node_exporter" {
   }
 }
 
+
+
+
+# Would provide metrics about the cluster itself 
+# Can't be tested in AWS Lab
+
+resource "kubernetes_service_account" "kube_state_metrics" {
+    count = var.enable_monitoring && var.enable_kube_state_metrics ? 1 : 0
+  depends_on = [ 
+    kubernetes_namespace.prometheus
+   ]
+
+  metadata {
+    name = "kube-state-metrics"
+    namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+  }
+}
+
+resource "kubernetes_cluster_role" "kube_state_metrics" {
+    count = var.enable_monitoring && var.enable_kube_state_metrics ? 1 : 0
+  metadata {
+    name = "kube-state-metrics"
+    labels = {
+      "app.kubernetes.io/component" = "exporter"
+      "app.kubernetes.io/name" = "kube-state-metrics"
+      "app.kubernetes.io/version" = "2.10.0"
+    }
+  }
+
+  rule {
+    api_groups = [""]
+    resources = [
+      "configmaps",
+      "secrets",
+      "nodes",
+      "pods",
+      "services",
+      "serviceaccounts",
+      "resourcequotas",
+      "replicationcontrollers",
+      "limitranges",
+      "persistentvolumeclaims",
+      "persistentvolumes",
+      "namespaces",
+      "endpoints",
+    ]
+    verbs = ["list", "watch"]
+  }
+    rule {
+    api_groups = ["apps"]
+    resources = [
+      "statefulsets",
+      "daemonsets",
+      "deployments",
+      "replicasets",
+    ]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["batch"]
+    resources = ["cronjobs", "jobs"]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["autoscaling"]
+    resources = ["horizontalpodautoscalers"]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["authentication.k8s.io"]
+    resources = ["tokenreviews"]
+    verbs = ["create"]
+  }
+   rule {
+    api_groups = ["authorization.k8s.io"]
+    resources = ["subjectaccessreviews"]
+    verbs = ["create"]
+  }
+
+  rule {
+    api_groups = ["policy"]
+    resources = ["poddisruptionbudgets"]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["certificates.k8s.io"]
+    resources = ["certificatesigningrequests"]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["discovery.k8s.io"]
+    resources = ["endpointslices"]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["storage.k8s.io"]
+    resources = ["storageclasses", "volumeattachments"]
+    verbs = ["list", "watch"]
+  }
+    rule {
+    api_groups = ["admissionregistration.k8s.io"]
+    resources = [
+      "mutatingwebhookconfigurations",
+      "validatingwebhookconfigurations",
+    ]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources = ["networkpolicies", "ingressclasses", "ingresses"]
+    verbs = ["list", "watch"]
+  }
+
+  rule {
+    api_groups = ["coordination.k8s.io"]
+    resources = ["leases"]
+    verbs = ["list", "watch"]
+  }
+  rule {
+    api_groups = ["rbac.authorization.k8s.io"]
+    resources = [
+      "clusterrolebindings",
+      "clusterroles",
+      "rolebindings",
+      "roles",
+    ]
+    verbs = ["list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "kube_state_metrics" {
+    count = var.enable_monitoring && var.enable_kube_state_metrics ? 1 : 0
+    metadata {
+    name = "kube-state-metrics"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind     = "ClusterRole"
+    name     = kubernetes_cluster_role.kube_state_metrics[0].metadata[0].name
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = kubernetes_service_account.kube_state_metrics[0].metadata[0].name
+    namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+  }
+}
+
+
+resource "kubernetes_deployment" "kube_state_metrics" {
+    count = var.enable_monitoring && var.enable_kube_state_metrics ? 1 : 0
+  metadata {
+    name = "kube-state-metrics"
+    namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+    labels = {
+      "app.kubernetes.io/component" = "exporter",
+      "app.kubernetes.io/name" = "kube-state-metrics",
+      "app.kubernetes.io/version" = "2.10.0",
+      "prometheus.io/cluster" = "true"
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "kube-state-metrics"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/component" = "exporter",
+          "app.kubernetes.io/name" = "kube-state-metrics",
+          "app.kubernetes.io/version" = "2.10.0",
+          "prometheus.io/cluster" = "true"
+        }
+      }
+      spec {
+        node_selector = {
+          "kubernetes.io/os" = "linux"
+
+        }
+        service_account_name = kubernetes_service_account.kube_state_metrics[0].metadata[0].name
+        container {
+          image = "registry.k8s.io/kube-state-metrics/kube-state-metrics:v2.10.0"
+          liveness_probe {
+            http_get {
+              path = "/healthz"
+              port = 8080
+            }
+            initial_delay_seconds = 5
+            timeout_seconds = 5
+          }
+          name = "kube-state-metrics"
+          port {
+            container_port = 8080
+            name = "http-metrics"
+          }
+
+          port {
+            container_port = 8081
+            name = "telemetry"
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = 8081
+            }
+            initial_delay_seconds = 5
+            timeout_seconds = 5
+          }
+
+          security_context {
+            allow_privilege_escalation = false
+            capabilities {
+              drop = ["all"]
+            }
+            read_only_root_filesystem = true
+            run_as_non_root = true
+            run_as_user = 65534
+          }
+        }
+      }
+    }
+    
+  } 
+}
+
+resource "kubernetes_service" "kube_state_metrics" {
+    count = var.enable_monitoring && var.enable_kube_state_metrics ? 1 : 0
+    metadata {
+      name = "kube-state-metrics"
+      namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+      labels = {
+        "app.kubernetes.io/component" = "exporter",
+        "app.kubernetes.io/name" = "kube-state-metrics",
+        "app.kubernetes.io/version" = "2.10.0",
+      }
+    }
+
+    spec {
+      cluster_ip = "None"
+      port {
+        name = "http-metrics"
+        port = 8080
+        target_port = 8080
+      }
+
+      port {
+        name = "telemetry"
+        port = 8081
+        target_port = 8081
+      }
+  selector = {
+    "app.kubernetes.io/name" = "kube-state-metrics"
+  }   
+}
+}
