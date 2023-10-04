e5aefe8baa8f9edf01d42410e68be505416defbb
danci1050 <obilesa@gmail.com>
2023-10-04 13:52:30 +0100

    Update prometheus.tf

--- prometheus.tf
+++ prometheus.tf
@@ -92,3 +92,135 @@ resource "kubernetes_service_account" "prometheus" {
     namespace = kubernetes_namespace.prometheus.metadata[0].name
   }
 }
+
+
+# deploys prometheus pod
+resource "kubernetes_deployment" "prometheus" {
+  depends_on = [ 
+    kubernetes_namespace.prometheus,
+    kubernetes_service_account.prometheus,
+    kubernetes_config_map.prometheus-config,
+    kubernetes_cluster_role.prometheus
+   ]
+
+  metadata {
+    name = "prometheus"
+    labels = {
+      app = "prometheus"
+    }
+    namespace = kubernetes_namespace.prometheus.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "prometheus"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "prometheus"
+        }
+      }
+
+      spec {
+        service_account_name = kubernetes_service_account.prometheus.metadata[0].name
+        automount_service_account_token = true
+        container {
+          name = "prometheus"
+          image = "prom/prometheus:v2.45.0"
+          args = []
+
+          port {
+            container_port = 9090
+          }
+
+          volume_mount {
+            name       = "config"
+            mount_path = "/etc/prometheus"
+          }
+
+          volume_mount {
+            name       = "prometheus-storage"
+            mount_path = "/prometheus"
+          }
+        }
+
+        volume {
+          name = "config"
+          config_map {
+            name = kubernetes_config_map.prometheus-config.metadata[0].name
+          }
+        }
+
+        volume {
+          name = "prometheus-storage"
+          empty_dir {}
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_service" "prometheus" {
+    metadata {
+        name = "prometheus-lb"
+        namespace = "monitoring"
+    }
+    
+    spec {
+        selector = {
+        app = kubernetes_deployment.prometheus.spec[0].template[0].metadata[0].labels.app
+        }
+    
+        port {
+        port        = 9090
+        target_port = 9090
+        }
+    
+        type = "NodePort"
+    }
+}
+
+
+resource "kubernetes_ingress_v1" "prometheus" {
+  metadata {
+    name = "prometheus"
+    labels = {
+      app = "prometheus"
+    }
+    namespace = kubernetes_namespace.prometheus.metadata[0].name
+
+    annotations = {
+      "alb.ingress.kubernetes.io/scheme" = "internet-facing"
+      "alb.ingress.kubernetes.io/group.name" = "outbound"
+      "alb.ingress.kubernetes.io/group.order" = "1"
+      "alb.ingress.kubernetes.io/listen-ports" = "[{\"HTTP\": 9090}]"
+    }
+  }
+
+  spec {
+    ingress_class_name = "alb"
+
+    rule {
+      http {
+        path {
+          path = "/*"
+          backend {
+            service {
+              name = kubernetes_service.prometheus.metadata[0].name
+              port {
+                number = kubernetes_service.prometheus.spec[0].port[0].port
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
