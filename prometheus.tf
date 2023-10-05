80268bd5ca7c0d3be21a0b850c700ac49067f9ee
danci1050 <obilesa@gmail.com>
2023-10-05 13:41:27 +0100

    Update prometheus.tf

--- prometheus.tf
+++ prometheus.tf
@@ -534,3 +534,214 @@ resource "kubernetes_deployment" "kube_state_metrics" {
   } 
 }
 
+
+resource "kubernetes_service" "kube_state_metrics" {
+    count = var.enable_monitoring && var.enable_kube_state_metrics ? 1 : 0
+    metadata {
+      name = "kube-state-metrics"
+      namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+      labels = {
+        "app.kubernetes.io/component" = "exporter",
+        "app.kubernetes.io/name" = "kube-state-metrics",
+        "app.kubernetes.io/version" = "2.10.0",
+      }
+    }
+
+    spec {
+      cluster_ip = "None"
+      port {
+        name = "http-metrics"
+        port = 8080
+        target_port = 8080
+      }
+
+      port {
+        name = "telemetry"
+        port = 8081
+        target_port = 8081
+      }
+  selector = {
+    "app.kubernetes.io/name" = "kube-state-metrics"
+  }   
+}
+}
+
+// Could be enhanced by using the the kublet's built in cadvisor
+resource "kubernetes_service_account" "cadvisor" {
+    count = var.enable_monitoring && var.enable_cadvisor_metrics ? 1 : 0
+    metadata {
+      name = "cadvisor"
+      namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+    }
+}
+
+resource "kubernetes_cluster_role" "cadvisor" {
+    count = var.enable_monitoring && var.enable_cadvisor_metrics ? 1 : 0
+  metadata {
+    name = "cadvisor"
+  }
+
+  rule {
+    api_groups = ["policy"]
+    resources  = ["podsecuritypolicies"]
+    verbs = ["use"]
+    resource_names = ["cadvisor"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "cadvisor" {
+  count = var.enable_monitoring && var.enable_cadvisor_metrics ? 1 : 0
+  metadata {
+    name = "cadvisor"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind     = "ClusterRole"
+    name     = kubernetes_cluster_role.cadvisor[0].metadata[0].name
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = kubernetes_service_account.cadvisor[0].metadata[0].name
+    namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+  }
+}
+resource "kubernetes_daemonset" "cadvisor" {
+    count = var.enable_monitoring && var.enable_cadvisor_metrics ? 1 : 0
+  metadata {
+    name      = "cadvisor"
+    namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        name = "cadvisor"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          name = "cadvisor"
+          "prometheus.io/cadvisor" = "true"
+        }
+      }
+
+      spec {
+        service_account_name = kubernetes_service_account.cadvisor[0].metadata[0].name
+
+        container {
+          name  = "cadvisor"
+          image = "gcr.io/cadvisor/cadvisor:latest"
+
+          volume_mount {
+            name      = "rootfs"
+            mount_path = "/rootfs"
+            read_only  = true
+          }
+
+          volume_mount {
+            name      = "var-run"
+            mount_path = "/var/run"
+            read_only  = true
+          }
+
+          volume_mount {
+            name      = "sys"
+            mount_path = "/sys"
+            read_only  = true
+          }
+
+          volume_mount {
+            name      = "docker"
+            mount_path = "/var/lib/docker"
+            read_only  = true
+          }
+
+          volume_mount {
+            name      = "disk"
+            mount_path = "/dev/disk"
+            read_only  = true
+          }
+
+          port {
+            name          = "http"
+            container_port = 8080
+            protocol      = "TCP"
+          }
+
+        }
+
+        automount_service_account_token = false
+        termination_grace_period_seconds = 30
+
+        volume {
+          name = "rootfs"
+
+          host_path {
+            path = "/"
+          }
+        }
+
+        volume {
+          name = "var-run"
+
+          host_path {
+            path = "/var/run"
+          }
+        }
+
+        volume {
+          name = "sys"
+
+          host_path {
+            path = "/sys"
+          }
+        }
+
+        volume {
+          name = "docker"
+
+          host_path {
+            path = "/var/lib/docker"
+          }
+        }
+
+        volume {
+          name = "disk"
+
+          host_path {
+            path = "/dev/disk"
+          }
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_service" "cadvisor" {
+    count = var.enable_monitoring && var.enable_cadvisor_metrics ? 1 : 0
+  metadata {
+    name = "cadvisor"
+    namespace = kubernetes_namespace.prometheus[0].metadata[0].name
+  }
+
+  spec {
+    selector = {
+      name = "cadvisor"
+    }
+
+    port {
+      name = "http"
+      port = 8080
+      target_port = 8080
+    }
+  }
+}
+
+
+
+
